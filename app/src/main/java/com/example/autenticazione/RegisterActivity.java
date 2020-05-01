0655922c5160cc099a1055f3ccc1fd7a422a8457
SilviaDG9 <missfreezer@gmail.com>
2020-05-01 16:50:50 +0200

    con upload immagine prov

--- app/src/main/java/com/example/autenticazione/RegisterActivity.java
+++ app/src/main/java/com/example/autenticazione/RegisterActivity.java
@@ -2,12 +2,23 @@ package com.example.autenticazione;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,13 +28,22 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "RegisterActivity";
+    private static final int PERMISSION_SAVE_FILE = 3;
     EditText nome;
     EditText email;
     EditText password;
     EditText confermaPassword;
+    ImageView aggiungiImmagine;
+    private final int PICK_IMAGE_CAMERA = 1, PICK_IMAGE_GALLERY = 2;
+    private InputStream inputStreamImg;
+    private Bitmap bitmap;
+    private Bitmap rotated_bitmap;
 
     //Firebase
     private FirebaseAuth mAuth;
@@ -43,6 +63,12 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         initUI();
 
+        aggiungiImmagine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
     }
     public void initUI(){
         //ciao a tutti ok
@@ -50,6 +76,7 @@ public class RegisterActivity extends AppCompatActivity {
          email = (EditText) findViewById(R.id.et_email);
          password = (EditText) findViewById(R.id.et_password);
          confermaPassword = (EditText) findViewById(R.id.et_confermaPassword);
+         aggiungiImmagine = (ImageView) findViewById(R.id.ivUploadImage);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -154,5 +181,75 @@ public class RegisterActivity extends AppCompatActivity {
         else
             return false;
     }
+
+    private void selectImage() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        PERMISSION_SAVE_FILE);
+            }
+        } else {
+            start();
+        }
+    }
+
+
+    private void start() {
+        final CharSequence[] options = {"Scatta foto", "Scegli dalla galleria", "Annulla"};
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Scegli un'opzione");
+        alertDialog.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Scatta foto")) {
+                    dialog.dismiss();
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, PICK_IMAGE_CAMERA);
+                } else if (options[item].equals("Scegli dalla galleria")) {
+                    dialog.dismiss();
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, PICK_IMAGE_GALLERY);
+                } else if (options[item].equals("Annulla")) {
+                    dialog.dismiss();
+                }
+            }
+            });
+        alertDialog.show();
+     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        inputStreamImg = null;
+        if (requestCode == PICK_IMAGE_CAMERA) {
+
+            Uri selectedImage = data.getData();
+            bitmap = (Bitmap) data.getExtras().get("data");
+            rotated_bitmap= RotateBitmap(bitmap,90);
+            aggiungiImmagine.setImageBitmap(rotated_bitmap);
+
+        } else if (requestCode == PICK_IMAGE_GALLERY) {
+
+            Uri selectedImage = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            aggiungiImmagine.setImageBitmap(bitmap);
+        }
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
 }
 
